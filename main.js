f939c7b2a3fc610d434dc309ca0f103d95206ecf
felix-richard <105433778+felix-richard@users.noreply.github.com>
2023-02-27 17:31:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,6 +9,25 @@ canvas.center();
 video = createCapture(VIDEO);
 video.size(300,300);
 video.hide();
+poseNet= ml5.poseNet(video,modelloaded);
+poseNet.on('pose',gotPoses);
 }
 function draw()
-{}
+{
+    image(video,0,0,300,300);
+}
+
+
+function modelloaded()
+{
+    console.log('poseNet is intialized');
+}
+function gotPoses(results)
+{
+if(results.lenght>0)
+{
+    console.log(results);
+    console.log("nose x ="+results[0].pose.nose.x);
+    console.log("nose y ="+results[0].pose.nose.y);
+}
+}
