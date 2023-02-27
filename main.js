7a95229ee5854f57a65f1d1d5ca5b3e85f0afdd6
felix-richard <105433778+felix-richard@users.noreply.github.com>
2023-02-27 17:55:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+noseX=0
+noseY=0
+
 function preload() 
 {
+    filterimg=loadImage('https://i.postimg.cc/0yC5V69X/image-removebg-preview-1.png');
 }
 
 function setup()
@@ -15,6 +19,7 @@ poseNet.on('pose',gotPoses);
 function draw()
 {
     image(video,0,0,300,300);
+    image(filterimg,noseX-20,noseY+5,50,50);
 }
 
 
@@ -24,10 +29,12 @@ function modelloaded()
 }
 function gotPoses(results)
 {
-if(results.lenght>0)
+if(results.length>0)
 {
     console.log(results);
-    console.log("nose x ="+results[0].pose.nose.x);
-    console.log("nose y ="+results[0].pose.nose.y);
+    noseX=results[0].pose.nose.x;
+    noseY=results[0].pose.nose.y;
+    console.log("nose x ="+noseX);
+    console.log("nose y ="+noseY);
 }
 }
